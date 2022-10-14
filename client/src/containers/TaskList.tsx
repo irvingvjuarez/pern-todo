f5fcbd1c471e6d3b8dc0719843a8b5ad61aa91f3
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-14 13:53:29 -0500

    Handling when no tasks exist

--- client/src/containers/TaskList.tsx
+++ client/src/containers/TaskList.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+
 export const TaskList = () => {
+	const [tasks, setTasks] = useState([])
+
+	useEffect(() => {
+		fetch("http://localhost:3000/api/v1/tasks")
+			.then(res => res.json())
+			.then(data => setTasks(data))
+	}, [])
+
 	return (
 		<section className="section">
-			<div className="w-full grid grid-cols-[1fr 85px] justify-between p-1 border-b-2 border-sub-dark rounded-sm">
-				<h2>Task</h2>
-			</div>
+			{tasks.length > 0 ? (
+				<>
+					<div className="w-full grid grid-cols-[1fr_85px] justify-between p-1 border-b-2 border-sub-dark rounded-sm">
+						<h2>Task</h2>
+					</div>
+					<p>There are tasks</p>
+				</>
+			) : (
+				<span className="my-3 block">
+					No tasks available.
+				</span>
+			)}
 		</section>
 	)
 }
