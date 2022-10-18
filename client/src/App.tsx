e34b6419fcace0edc0c00e31c5f24eefdbe2fb40
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-18 17:30:18 -0500

    Improvement of the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,33 +3,10 @@ import { InlineForm } from './components/InlineForm'
 import { InputBox } from './components/InputBox'
 import { Button } from './components/Button'
 import { TaskList } from './containers/TaskList'
-import { useState } from 'react'
-import { TASKS_API } from './globals'
+import { useTasks } from './hooks/useTasks'
 
 function App() {
-	const [task, setTask] = useState("")
-	const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		setTask(evt.target.value)
-	}
-	const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-		evt.preventDefault()
-
-		fetch(TASKS_API, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				"Accept": "application/json"
-			},
-			body: JSON.stringify({
-				content: task
-			})
-		}).then(res => res.json())
-			.then(data => {
-				console.log({
-					data
-				})
-			})
-	}
+	const { handleChange, handleSubmit } = useTasks()
 
   return (
     <div className="App">
--- client/src/hooks/useTasks.ts
+++ client/src/hooks/useTasks.ts
@@ -4,7 +4,35 @@ import { TASKS_API } from "../globals"
 
 export const useTasks = () => {
 	const [tasks, setTasks] = useState<Task[]>([])
+	const [taskInput, setTaskInput] = useState("")
 	const [loading, setLoading] = useState(true)
+
+	const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		setTaskInput(evt.target.value)
+	}
+
+	const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+		evt.preventDefault()
+
+		fetch(TASKS_API, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify({
+				content: taskInput
+			})
+		}).then(res => res.json())
+			.then(data => {
+				const newTask = data.rows[0]
+				setTaskInput("")
+				setTasks(prev => ({
+					...prev,
+					newTask
+				}))
+			})
+	}
 
 	useEffect(() => {
 		const controller = new AbortController()
@@ -22,6 +50,8 @@ export const useTasks = () => {
 
 	return {
 		tasks,
-		loading
+		loading,
+		handleChange,
+		handleSubmit
 	}
 }
