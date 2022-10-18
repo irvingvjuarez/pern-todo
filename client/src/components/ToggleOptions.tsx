31fec603ff6c53bc7530e45d667f97e7f4fc60ef
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-17 20:16:38 -0500

    Menu toggled correctly

--- client/src/components/ToggleOptions.tsx
+++ client/src/components/ToggleOptions.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react"
 import { SlOptionsVertical } from "react-icons/sl"
 
 export const ToggleOptions = () => {
-	const handleClick = () => {
-		console.log("Hello World")
-	}
+	const [open, setOpen] = useState(false)
+	const toggleOpen = () => setOpen(prev => !prev)
 
 	return (
-		<div className="flex justify-center items-center">
+		<div className="flex justify-center items-center relative">
 			<SlOptionsVertical
 				className="text-center"
-				onClick={handleClick}
+				onClick={toggleOpen}
+				onBlur={toggleOpen}
 			/>
+
+			{open && (
+				<ul className="absolute right-full top-0 bg-sub-bg border-contrast border-2 p-2">
+					<li>Edit</li>
+					<li>Delete</li>
+				</ul>
+			)}
 		</div>
 	)
 }
