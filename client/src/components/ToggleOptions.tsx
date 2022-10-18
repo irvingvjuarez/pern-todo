af56a51885166557505fd75338c6aed89491b8d0
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-17 21:17:43 -0500

    Toggling menu working fine

--- client/src/components/ToggleOptions.tsx
+++ client/src/components/ToggleOptions.tsx
@@ -1,20 +1,33 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { SlOptionsVertical } from "react-icons/sl"
 
 export const ToggleOptions = () => {
+	const menuRef = useRef<HTMLUListElement | null>(null)
 	const [open, setOpen] = useState(false)
-	const toggleOpen = () => setOpen(prev => !prev)
+	const toggleOpen = () => setOpen(prev => {
+		if (!prev) {
+			setTimeout(() => {
+				menuRef.current?.focus()
+			}, 0)
+		}
+		return !prev
+	})
+	const handleClick = (evt: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		evt.stopPropagation()
+		toggleOpen()
+	}
 
 	return (
-		<div className="flex justify-center items-center relative">
-			<SlOptionsVertical
-				className="text-center"
-				onClick={toggleOpen}
-				onBlur={toggleOpen}
-			/>
+		<div className="flex justify-center items-center relative" onClick={handleClick}>
+			<SlOptionsVertical className="text-center"/>
 
 			{open && (
-				<ul className="absolute right-full top-0 bg-sub-bg border-contrast border-2 p-2">
+				<ul
+					tabIndex={-1}
+					ref={menuRef}
+					className="absolute right-full top-0 bg-sub-bg border-contrast border-2 p-2"
+					onBlur={toggleOpen}
+				>
 					<li>Edit</li>
 					<li>Delete</li>
 				</ul>
