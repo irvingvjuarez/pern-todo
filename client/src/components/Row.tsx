92afd994c07abb7b31f2aa1b82fb4e50a0cc90b8
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-18 16:51:28 -0500

    Responsiveness done in the Task table

--- client/src/components/Row.tsx
+++ client/src/components/Row.tsx
@@ -1,4 +1,6 @@
 import { ToggleOptions } from "../components/ToggleOptions"
+import { AiFillEdit } from "react-icons/ai"
+import { RiDeleteBin6Fill } from "react-icons/ri"
 
 interface RowProps {
 	content: string;
@@ -8,15 +10,21 @@ interface RowProps {
 export const Row: React.FC<RowProps> = ({ content, variant = "standard" }) => {
 	return (
 		<div
-			className="w-full grid grid-cols-[1fr_25px] justify-between p-1 border-b-2 border-sub-dark rounded-sm"
+			className="w-full grid grid-cols-[1fr_25px] justify-between p-1 border-b-2 border-sub-dark rounded-sm sm:grid-cols-[1fr_35px_60px]"
 		>
 			{variant === "standard" ? (
 				<>
 					<p>{content}</p>
-					<ToggleOptions />
+					<ToggleOptions className="sm:hidden" />
+					<AiFillEdit />
+					<RiDeleteBin6Fill />
 				</>
 			) : (
-				<h2>{content}</h2>
+				<>
+					<h2>{content}</h2>
+					<h3>Edit</h3>
+					<h3>Remove</h3>
+				</>
 			)}
 		</div>
 	)
--- client/src/components/ToggleOptions.tsx
+++ client/src/components/ToggleOptions.tsx
@@ -3,11 +3,14 @@ import { useToggleOptions } from "../hooks/useToggleOptions"
 import { ToggledMenu } from "./ToggledMenu"
 import { ConditionalNode } from "./ConditionalNode"
 
-export const ToggleOptions = () => {
+interface ToggleOptionsProps {
+	className?: string;
+}
+export const ToggleOptions: React.FC<ToggleOptionsProps> = ({ className }) => {
 	const { open, handleClick, toggleOpen, menuRef } = useToggleOptions()
 
 	return (
-		<div className="flex justify-center items-center relative" onClick={handleClick}>
+		<div className={`flex justify-center items-center relative ${className}`} onClick={handleClick}>
 			<SlOptionsVertical className="text-center"/>
 
 			<ConditionalNode condition={open}>
