6c9cc66590f9e0c103702ac293cac593e5b741a5
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-20 12:11:28 -0500

    Tasks Store working good already

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux"
 import { PageContainer } from './containers/PageContainer'
 import { InlineForm } from './components/InlineForm'
 import { InputBox } from './components/InputBox'
 import { Button } from './components/Button'
 import { TaskList } from './containers/TaskList'
 import { useTasks } from './hooks/useTasks'
+import { InitialState } from './stores/initialState'
 
 function App() {
 	const { handleChange, handleSubmit, taskInput } = useTasks()
+	const tasks = useSelector((state: InitialState) => state.tasks)
+
+	console.log({
+		tasks
+	})
 
   return (
     <div className="App">
