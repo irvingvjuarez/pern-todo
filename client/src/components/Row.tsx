d5205e90902ed4a73a503e6d401cbe2cb3331bda
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-20 16:28:24 -0500

    Refactoring Row component

--- client/src/components/Row.tsx
+++ client/src/components/Row.tsx
@@ -4,6 +4,7 @@ import { AiFillEdit } from "react-icons/ai"
 import { RiDeleteBin6Fill } from "react-icons/ri"
 import { TaskContext } from "../contexts/taskContext";
 import { useTasks } from "../hooks/useTasks";
+import { ConditionalNode } from "../components/ConditionalNode"
 
 interface RowProps {
 	content: string;
@@ -19,27 +20,25 @@ export const Row: React.FC<RowProps> = ({ content, id, variant = "standard" }) =
 			<div
 				className="w-full grid grid-cols-[1fr_25px] justify-between px-1 py-2 border-b-2 border-sub-dark rounded-sm sm:grid-cols-[1fr_35px_60px]"
 			>
-				{variant === "standard" ? (
-					<>
-						<p>{content}</p>
-						<ToggleOptions className="sm:hidden" />
+				<ConditionalNode condition={variant === "standard"}>
+					<p>{content}</p>
+					<ToggleOptions className="sm:hidden" />
 
-						<Icon
-							onClick={() => console.log("Hi")}
-							iconComponent={() => AiFillEdit}
-						/>
-						<Icon
-							onClick={() => deleteTask(id as number)}
-							iconComponent={() => RiDeleteBin6Fill}
-						/>
-					</>
-				) : (
-					<>
-						<h2>{content}</h2>
-						<h3 className="hidden sm:block">Edit</h3>
-						<h3 className="hidden sm:block">Remove</h3>
-					</>
-				)}
+					<Icon
+						onClick={() => console.log("Hi")}
+						iconComponent={() => AiFillEdit}
+					/>
+					<Icon
+						onClick={() => deleteTask(id as number)}
+						iconComponent={() => RiDeleteBin6Fill}
+					/>
+				</ConditionalNode>
+
+				<ConditionalNode condition={variant !== "standard"}>
+					<h2>{content}</h2>
+					<h3 className="hidden sm:block">Edit</h3>
+					<h3 className="hidden sm:block">Remove</h3>
+				</ConditionalNode>
 			</div>
 		</TaskContext.Provider>
 	)
