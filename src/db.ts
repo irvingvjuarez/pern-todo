6223df6a3e0b9d5c8738ad481f1abca103b5ec10
Irving Juárez <irvingjuarez274@gmail.com>
2022-10-23 12:34:29 -0500

    Change env var name to avoid conflicts with global user of the OS

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import { Pool } from "pg"
 config()
 
 export const db = new Pool({
-	user: process.env.USER,
+	user: process.env.DB_USER,
 	password: process.env.PASSWORD,
 	host: process.env.HOST,
 	port: Number(process.env.PORT) || 5432,
